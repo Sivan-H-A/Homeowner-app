b167885a17aa1ab19f850f5c198853e3976d1e6f
Sivan-H-A <sivananaf@gmail.com>
2021-05-10 00:00:41 +0300

    creating NotFound page

---
+++ src/pages/NotFoundPage/NotFoundPage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Container } from 'react-bootstrap'
+
+export default function NotFoundPage() {
+    return (
+        <Container>
+            <img src="https://i.stack.imgur.com/6M513.png" style={{"width":"100%"}} alt="Not Found"></img>
+        </Container>
+    )
+}
