f990a185e56964c02d668989fe2bcd352d7c7715
Sivan-H-A <sivananaf@gmail.com>
2021-05-13 10:13:03 +0300

    adding login process

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -4,54 +4,61 @@ import image from '../../assets/background-image.jpg';
 import LandingPageCardComponent from '../../components/LandingPageCardComponent/LandingPageCardComponent';
 import { Alert, Button, Container, Form } from 'react-bootstrap';
 import ActiveUserContext from '../../shared/ActiveUserContext';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
+import BackendDataService from '../../utils/BackendDataService';
 
-export default function Homepage() {
+export default function Homepage({onLogin}) {
     const [showInvalidLogin, setShowInvalidLogin] = useState(false);
     const [email, setEmail] = useState("");
     const [pwd, setPwd] = useState("");
     const activeUser = useContext(ActiveUserContext);
 
+    // if (activeUser) {
+    //     return <Redirect to="/dashboard"/>
+    // }
+
     async function login(e) {
         e.preventDefault();
-
-        // try {
-        //     const activeUser = await UserModel.login(email, pwd);
-        //     onLogin(activeUser);
-        // } catch (error) {
-        //     console.error('Error while logging in user', error);
-        //     setShowInvalidLogin(true);
-        // }
+        let activeUser = null;
+        try {
+            activeUser = await BackendDataService.login(email,pwd);
+            onLogin(activeUser);
+        } catch (error) {
+            console.error('Error while logging in user', error);
+            setShowInvalidLogin(true);
+        }
     }
     return (
         <Container fluid className="p-home">
             <div className="p-home-header">
-                <div className="p-home-title col-sm-6 col-md-8">
+                <div className={!activeUser? `p-home-title col-sm-6 col-md-8`: `p-home-title col-12`}>
                     <h1 className="display-4">Homeowner Association Management System</h1>
                     <h3>All you need to handle your building </h3>
                 </div>
-                <div className="p-login col-md-3">
-                    <h4>Login to Your Building</h4>
-                    <p>or <Link to="/signup">create a new committee</Link></p>
-                    {showInvalidLogin ? <Alert variant="danger">Invalid Credentials!</Alert> : null}
-                    <Form onSubmit={login} className="mb-3">                      
-                        <Form.Group className="mb-3" controlId="formBasicEmail">
-                            <Form.Control type="email" placeholder="Enter email address" 
-                                value={email} onChange={e => setEmail(e.target.value)} />
-                            <Form.Text className="text-muted">
-                            We'll never share your email with anyone else.
-                            </Form.Text>
-                        </Form.Group>
+                {!activeUser ?
+                    <div className="p-login col-md-3">
+                        <h4>Login to Your Building</h4>
+                        <p>or <Link to="/signup">create a new committee</Link></p>
+                        {showInvalidLogin ? <Alert variant="danger">Invalid Credentials!</Alert> : null}
+                        <Form onSubmit={login} className="mb-3">                      
+                            <Form.Group className="mb-3" controlId="formBasicEmail">
+                                <Form.Control type="email" placeholder="Enter email address" 
+                                    value={email} onChange={e => setEmail(e.target.value)} />
+                                <Form.Text className="text-muted">
+                                We'll never share your email with anyone else.
+                                </Form.Text>
+                            </Form.Group>
 
-                        <Form.Group className="mb-3" controlId="formBasicPassword">
-                            <Form.Control type="password" placeholder="Password" 
-                                value={pwd} onChange={e => setPwd(e.target.value)} />
-                        </Form.Group>
-                        <Button  className="mb-3" variant="primary" type="submit" block>
-                            Login
-                        </Button>
-                    </Form>
-                </div>
+                            <Form.Group className="mb-3" controlId="formBasicPassword">
+                                <Form.Control type="password" placeholder="Password" 
+                                    value={pwd} onChange={e => setPwd(e.target.value)} />
+                            </Form.Group>
+                            <Button  className="mb-3" variant="primary" type="submit" block>
+                                Login
+                            </Button>
+                        </Form>
+                    </div>
+                :null }
             </div>
             <div className="p-home-bg-img">
                 <img  className="p-home-img" src={image}></img>
