3f4d4af975deac015183b473ba732aa1c8b42e60
Sivan-H-A <sivananaf@gmail.com>
2021-05-18 13:34:02 +0300

    adding route for tenants, dashboard, messages + handle loading active user

--- src/App.js
+++ src/App.js
@@ -2,30 +2,35 @@ import { useState } from 'react';
 import { HashRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 import HomeownerNavbar from './components/HomeownerNavbar/HomeownerNavbar';
+import DashboardPage from './pages/DashboardPage/DashboardPage';
 import Homepage from './pages/HomePage/HomePage';
+import TenantsPage from './pages/TenantsPage/TenantsPage';
+import MessagesPage from './pages/MessagesPage/MessagesPage';
 import NotFoundPage from './pages/NotFoundPage/NotFoundPage';
 import SignupPage from './pages/SignupPage/SignupPage';
+import ActiveUserContext from './shared/ActiveUserContext'
+import BackendDataService from './utils/BackendDataService';
 
 function App() {
 
-  const [activeUser, setActiveUser] = useState(null)
+  const [activeUser, setActiveUser] = useState(BackendDataService.loadActiveUser());
   
   return (
-    <div className="App">
-      <HashRouter>
-        {/* <HomeownerNavbar/> */}
-        <Switch>
-          <Route exact path="/" >
-            <HomeownerNavbar onLogout={() => setActiveUser(null)}/>
-            <Homepage/></Route>
-          {/* <Route exact path="/login"><LoginPage onLogin={user => setActiveUser(user)}/></Route>*/}
-          <Route exact path="/signup">
-            <HomeownerNavbar onLogout={() => setActiveUser(null)}/>
-            <SignupPage onLogin={user => setActiveUser(user)}/></Route> 
-          <Route path="/"><NotFoundPage/></Route>          
-        </Switch>        
-      </HashRouter>
-    </div>
+    <ActiveUserContext.Provider value={activeUser}>
+      <div className="App">
+        <HashRouter>
+          <HomeownerNavbar onLogout={() => setActiveUser(null)}/>
+          <Switch>
+            <Route exact path="/"><Homepage onLogin={user => setActiveUser(user)} /></Route>
+            <Route exact path="/tenants"><TenantsPage/></Route>
+            <Route exact path="/signup"><SignupPage onLogin={user => setActiveUser(user)}/></Route> 
+            <Route exact path="/dashboard"><DashboardPage/></Route>
+            <Route exact path="/messages"><MessagesPage/></Route>
+            <Route path="/"><NotFoundPage/></Route>          
+          </Switch>        
+        </HashRouter>
+      </div>
+    </ActiveUserContext.Provider>
   );
 }
 
